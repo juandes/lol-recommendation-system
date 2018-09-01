e80812f880fb2df54a19fb06c3b5c9e6f562c691
Juan De Dios Santos <juan.dediossantos@lovoo.com>
2018-09-01 21:46:12 +0200

    Added shuffle to recommendations

--- recommender/recommender.go
+++ recommender/recommender.go
@@ -2,7 +2,9 @@ package recommender
 
 import (
 	"fmt"
+	"math/rand"
 	"sort"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/juandes/knn-recommender-system/distances"
@@ -28,8 +30,8 @@ func NewNeighborhoodBasedRecommender(data [][]float64, neighbors int) *Neighborh
 }
 
 // Recommend recommends the n number of items that are closer to a given vector using a given distance measure
-func (nbr *NeighborhoodBasedRecommender) Recommend(items []float64, numItemsToRecommend int, distanceMeasure distances.Distance) ([]Recommendation, error) {
-	recommendations, err := nbr.findKNearestNeighbors(items, numItemsToRecommend, distanceMeasure)
+func (nbr *NeighborhoodBasedRecommender) Recommend(items []float64, numItemsToRecommend int, distanceMeasure distances.Distance, shuffle bool) ([]Recommendation, error) {
+	recommendations, err := nbr.findKNearestNeighbors(items, numItemsToRecommend, distanceMeasure, shuffle)
 	if err != nil {
 		return nil, fmt.Errorf("Error encountered while finding K nearest neighbors: %v", err)
 	}
@@ -37,14 +39,31 @@ func (nbr *NeighborhoodBasedRecommender) Recommend(items []float64, numItemsToRe
 	return recommendations, nil
 }
 
-func (nbr *NeighborhoodBasedRecommender) findKNearestNeighbors(items []float64, n int, distanceMeasure distances.Distance) ([]Recommendation, error) {
+func (nbr *NeighborhoodBasedRecommender) findKNearestNeighbors(items []float64, n int, distanceMeasure distances.Distance, shuffle bool) ([]Recommendation, error) {
 	var (
 		d                 float64
 		err               error
 		distancesFromUser []Recommendation
+		order             []int
 	)
 
-	for i, user := range nbr.data {
+	order = make([]int, len(nbr.data))
+	for i := range order {
+		order[i] = i
+	}
+
+	// the point of shuffling the order in which
+	// the distances will be calculated
+	// is to avoid having always the same
+	// predictions in case all the n results
+	// to return have the same distance
+	if shuffle {
+		rand.Seed(time.Now().UnixNano())
+		rand.Shuffle(len(order), func(i, j int) { order[i], order[j] = order[j], order[i] })
+	}
+
+	for i, val := range order {
+		user := nbr.data[val]
 		if len(user) != nbr.numberItems {
 			return nil, fmt.Errorf("Incorrect number of items in vector")
 		}
@@ -57,7 +76,7 @@ func (nbr *NeighborhoodBasedRecommender) findKNearestNeighbors(items []float64, 
 		}
 
 		if err != nil {
-			return nil, fmt.Errorf("Error calculating distance")
+			return nil, fmt.Errorf("Error calculating distance: %v", err)
 		}
 
 		distancesFromUser = append(distancesFromUser, Recommendation{
