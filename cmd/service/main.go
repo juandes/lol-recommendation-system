493ab726b356c9d054271acf48b82245a59b196f
Juan De Dios Santos <juan.dediossantos@lovoo.com>
2018-10-23 22:14:06 +0200

    Changed output of recommendations and changed port

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -10,16 +10,20 @@ import (
 	"github.com/spf13/pflag"
 	"io/ioutil"
 	"net/http"
-	"strconv"
+	"os"
+	"strings"
 )
 
 var (
-	championsFile = pflag.String("championsmapping", "../../data/champions_key.json", "path to file containing the champions mapping")
-	trainingData  = pflag.String("trainingset", "../../data/winning_teams.csv", "path to training dataset")
-	champions     map[string]string
+	trainingData = pflag.String("trainingset", "../../data/winning_teams.csv", "path to training dataset")
+	// Had to read the PORT from an env variable because of Heroku
+	port = pflag.String("port", os.Getenv("PORT"), "listening port")
+
+	championToIndex = make(map[string]int)
+	indexToChampion = make(map[int]string)
 )
 
-// PredictionInput is the object to which the JSON obtain to predict, will be unmarshal to
+// PredictionInput is the object to which the inputted JSON will be unmarshaled to
 type PredictionInput struct {
 	Champions   []string `json:"champions"`
 	Intercept   bool     `json:"intercept"`
@@ -27,9 +31,9 @@ type PredictionInput struct {
 	Serendipity bool     `json:"serendipity"`
 }
 
-// Output is the return response that holds the recommendations
+// Output is the return response that contains the recommendations
 type Output struct {
-	Recommendations []recommender.Recommendation `json:"recommendations"`
+	Recommendations [][]string `json:"recommendations"`
 }
 
 // TODO (Juan): add an error structure. See this: https://blog.restcase.com/rest-api-error-codes-101/
@@ -39,22 +43,26 @@ type Output struct {
 
 func main() {
 	pflag.Parse()
-	// load the file with the mapping of champions name to index position
-	file, err := ioutil.ReadFile(*championsFile)
-	if err != nil {
-		log.Fatalf("Error reading champions file: %v", err)
+	if *port == "" {
+		*port = "8080"
 	}
-	err = json.Unmarshal(file, &champions)
 
 	// read the training set
-	train, _, err := data.ReadData(*trainingData)
+	train, header, err := data.ReadData(*trainingData)
 	if err != nil {
 		log.Fatalf("Error reading training set: %v", err)
 	}
 
-	log.Info("Starting recommendation service...")
+	for i, val := range header {
+		name := strings.ToLower(val)
+		championToIndex[name] = i
+		indexToChampion[i] = name
+	}
 
-	// create the recommender engine with k = 10
+	log.Info("Starting recommendation service...")
+	log.Infof("Listening on port %s", *port)
+
+	// create the recommender engine with k = 5
 	engine := recommender.NewNeighborhoodBasedRecommender(train, 5)
 
 	mux := http.NewServeMux()
@@ -63,13 +71,13 @@ func main() {
 	})
 	mux.HandleFunc("/recommend", recommendationHandler(engine))
 
-	go log.Fatal(http.ListenAndServe(":8080", mux))
+	go log.Fatal(http.ListenAndServe(":"+*port, mux))
 }
 
 func recommendationHandler(engine *recommender.NeighborhoodBasedRecommender) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		var pinput PredictionInput
-		input := make([]float64, len(champions))
+		input := make([]float64, len(championToIndex))
 
 		if r.Method != "POST" {
 			http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
@@ -81,6 +89,7 @@ func recommendationHandler(engine *recommender.NeighborhoodBasedRecommender) htt
 			http.Error(w, "Error reading request body",
 				http.StatusInternalServerError)
 		}
+
 		err = json.Unmarshal(body, &pinput)
 		if err != nil {
 			// TODO: Replace logs for http.Error
@@ -88,16 +97,15 @@ func recommendationHandler(engine *recommender.NeighborhoodBasedRecommender) htt
 			return
 		}
 
-		// create the feature vector by adding 1
+		// create the feature vector by writing 1
 		// in the index corresponding to the champion
 		for _, val := range pinput.Champions {
-			item, ok := champions[val]
+			item, ok := championToIndex[val]
 			if !ok {
 				log.Warningf("Unknown champion: %v", val)
 				return
 			}
-			championIdx, _ := strconv.Atoi(item)
-			input[championIdx] = 1
+			input[item] = 1
 		}
 
 		recommendations, err := engine.Recommend(input, vectormath.Pearson, pinput.Intercept, pinput.Shuffle, pinput.Serendipity)
@@ -106,8 +114,21 @@ func recommendationHandler(engine *recommender.NeighborhoodBasedRecommender) htt
 			return
 		}
 
+		allRecommendations := [][]string{}
+		for _, val := range recommendations {
+			recommendedItem := []string{}
+			for i, val2 := range val.GetRecommendation() {
+				if val2 != 1 {
+					continue
+				}
+				champ := indexToChampion[int(i)]
+				recommendedItem = append(recommendedItem, champ)
+			}
+			allRecommendations = append(allRecommendations, recommendedItem)
+		}
+
 		response, _ := json.Marshal(&Output{
-			Recommendations: recommendations,
+			Recommendations: allRecommendations,
 		})
 		fmt.Fprintln(w, string(response))
 	})
