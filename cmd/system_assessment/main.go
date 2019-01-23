8a0ad1e1b6edd6cab0a336f2e950e662239ce11c
Juan De Dios Santos <juan.dediossantos@lovoo.com>
2019-01-23 19:23:32 +0100

    Added plots, Rscript and missing datasets

--- cmd/system_assessment/main.go
+++ cmd/system_assessment/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/csv"
-	"fmt"
 	"os"
 	"strconv"
 	"time"
@@ -23,18 +22,17 @@ func main() {
 	)
 
 	// read the training set
-	train, _, err := data.ReadData("static/winning_teams.csv")
+	train, _, err := data.ReadData("../../static/winning_teams.csv")
 	if err != nil {
 		log.Fatalf("Error reading data: %v", err)
 	}
 
 	// load the testing set; the data that will be used to assess the system
-	test, _, err := data.ReadData("static/random_teams.csv")
+	test, _, err := data.ReadData("../../static/random_teams.csv")
 	if err != nil {
 		log.Fatalf("Error reading data: %v", err)
 	}
 
-	// create the recommender engine with k = 10
 	reco := recommender.NewNeighborhoodBasedRecommender(train, 10)
 
 	for _, val := range test {
@@ -47,31 +45,25 @@ func main() {
 			log.Fatalf("Error while recommending: %v", err)
 		}
 
-		// Append the id of the prediction
-		// These values had to be changed to string, because Golang's csv library deals only with string
-		//result = append(result, string(total))
 		// Append the distance score
 		result = append(result, strconv.FormatFloat(recommendations[0].GetDistance(), 'f', 16, 64))
 		// Append the time it took to perform the prediction
 		result = append(result, strconv.FormatFloat(tSince, 'f', 6, 64))
-		/* In the testing set, each 4 rows represent one team, where the first row is the team with
-		only one champion, the second row is a team composition of two champions and so on.
-		Since in this example, we are analyzing the decrease of distance as more heroes are being
-		added to the team, I want to keep the distances related to one team in the same row,
-		that's why I am appending it to the results dataset every 4 iterations*/
-		if total%4 == 0 {
-			results = append(results, result)
-			fmt.Printf("result: %v\n", result)
-			result = []string{}
-
-		}
-
+		results  = append(results, result)
+		
 		if total%100 == 0 {
 			log.Infof("total: %d", total)
 		}
-	}
 
-	file, err := os.Create("data/assessment_02Pearson_Shuffle.csv")
+		result = []string{}
+	}
+	writeFile("../../appendix/assessments/distance_time.csv", results)
+}
+
+
+func writeFile(fileName string, data [][]string){
+	
+	file, err := os.Create(fileName)
 	if err != nil {
 		log.Fatalf("Unable to create file: %v", err)
 	}
@@ -80,9 +72,8 @@ func main() {
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 
-	err = writer.WriteAll(results)
+	err = writer.WriteAll(data)
 	if err != nil {
 		log.Fatalf("Unable to create file: %v", err)
 	}
-
 }
